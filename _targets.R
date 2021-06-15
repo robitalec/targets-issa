35c281e4a9c278761660285992eab20e691553d6
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-15 13:33:30 -0700

    add splits

--- _targets.R
+++ _targets.R
@@ -21,14 +21,33 @@ path <- file.path('input', 'test.csv')
 id <- 'id'
 datetime <- 'datetime'
 
+# Split by: within which column or set of columns (eg. c(id, yr))
+#  do we want to split our analysis?
+splitBy <- id
+
+
 # Targets -----------------------------------------------------------------
 list(
+	# Read input data
 	tar_target(
 		input,
 		fread(path)
 	),
+
+	# Remove duplicated id*datetime rows
 	tar_target(
 		mkunique,
 		unique(input, by = c(id, datetime))
-	)
+	),
+
+	# Set up split -- these are our iteration units
+	tar_target(
+		splits,
+		mkunique[, tar_group := .GRP, by = splitBy],
+		iteration = 'group'
+	),
+	tar_target(
+		splitsnames,
+		unique(mkunique[, .(path = path), by = splitBy])
+	),
 )
