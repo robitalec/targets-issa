e32cb28f1d23a85600c02033409d80631d42720a
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-16 14:48:33 -0700

    add make_unique_complete and extract_lc functions

--- R/functions.R
+++ R/functions.R
@@ -3,11 +3,19 @@
 
 
 
-# Read data ---------------------------------------------------------------
+
+# Make unique and complete ------------------------------------------------
+make_unique_complete <- function(DT, id, datetime, long, lat) {
+	na.omit(unique(input, by = c(id, datetime)),
+					cols = c(long, lat, datetime))
+}
 
 
 
-# Make tracks -------------------------------------------------------------
+# Extract land cover ------------------------------------------------------
+extract_lc <- function(DT, lc, lcvalues) {
+	merge(DT[, value := extract(lc, xy)], lcvalues, by = value)
+}
 
 
 
