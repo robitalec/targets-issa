dc918ede434dcaa848cf29ee1a94a57aa34039d7
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-16 16:07:14 -0700

    rm lonlat = TRUE

--- R/functions.R
+++ R/functions.R
@@ -24,11 +24,11 @@ extract_lc <- function(DT, lc, x, y, lcvalues) {
 
 # Resample with cancel check ---------------------------------------------------------
 resample_tracks <- function(tracks, rate, tolerance) {
-	t <- track_resample(tracks, rate = hours(rate), tolerance = minutes(tolerance)) %>%
+	t <- track_resample(tracks, rate = rate, tolerance = tolerance) %>%
 		filter_min_n_burst()
 
 	# Cancel if there are not at least three rows after resample
 	tar_cancel(nrow(t) < 3)
 
-	t %>% steps_by_burst(., lonlat = TRUE)
+	t %>% steps_by_burst(.)
 }
