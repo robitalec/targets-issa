6e72253dcf3a8bd8df635c116f83bb1d4fb3cb4e
Alec L. Robitaille <robit.alec@gmail.com>
2021-06-16 15:22:39 -0700

    add resample_tracks function with tar_cancel

--- R/functions.R
+++ R/functions.R
@@ -6,18 +6,29 @@
 
 # Make unique and complete ------------------------------------------------
 make_unique_complete <- function(DT, id, datetime, long, lat) {
-	na.omit(unique(input, by = c(id, datetime)),
+	na.omit(unique(DT, by = c(id, datetime)),
 					cols = c(long, lat, datetime))
 }
 
 
 
 # Extract land cover ------------------------------------------------------
-extract_lc <- function(DT, lc, lcvalues) {
-	merge(DT[, value := extract(lc, xy)], lcvalues, by = value)
+extract_lc <- function(DT, lc, x, y, lcvalues) {
+	merge(
+		DT[, value := raster::extract(lc, do.call(cbind, .SD)),
+			 .SDcols = c(x, y)],
+		lcvalues,
+		by = 'value',
+		all.x = TRUE)
 }
 
+# Resample with cancel check ---------------------------------------------------------
+resample_tracks <- function(tracks, rate, tolerance) {
+	t <- track_resample(tracks, rate = hours(rate), tolerance = minutes(tolerance)) %>%
+		filter_min_n_burst()
 
+	# Cancel if there are not at least three rows after resample
+	tar_cancel(nrow(t) < 3)
 
-
-
+	t %>% steps_by_burst(., lonlat = TRUE)
+}
