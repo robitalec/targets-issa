31ee95c6e70d495a39c4603d18108b16187ec38f
Julie Turner <julwturner@gmail.com>
2022-03-24 11:57:12 -0230

    make data.table

--- R/functions.R
+++ R/functions.R
@@ -127,5 +127,5 @@ make_step_id <- function(DT) {
 	if (is.null(DT)) return()
 	if (nrow(DT) == 0) return()
 
-	DT[,indiv_step_id := paste(id, step_id_, sep = '_')]
+	setDT(DT)[,indiv_step_id := paste(id, step_id_, sep = '_')]
 }
