6f2b14d0dcf00bac02748ca989ff036c0f58ea6f
Julie Turner <julwturner@gmail.com>
2022-03-24 11:57:42 -0230

    bahaha, documenting probs with toy model....

--- scripts/explore-results.R
+++ scripts/explore-results.R
@@ -1,4 +1,5 @@
 library(targets)
+library(glmmTMB)
 
 ## Run the full workflow
 tar_make()
@@ -15,10 +16,38 @@ tar_read(tracks)
 tar_read(distributions)
 
 # *Load* all random steps and extracted landscape value
-tar_load(mergelc)
+tar_load(stepID)
 
 
-glmmtmb(case_ ~ I(log(sl_)) + I(log(sl_)):tod_start + distto + distto:I(log(sl_)) +
-					(1|indiv_step_id) + ## NEED TO MAKE THIS
+mod.1 <- glmmTMB(case_ ~ I(log(sl_)) + I(log(sl_)):tod_start_ + distto + distto:I(log(sl_)) +
+					(1|indiv_step_id) +
 					(0+ I(log(sl_))|id) +
-					(0+))
+					(0+ I(log(sl_)):tod_start_|id) +
+					(0+ distto) +
+					(0+ distto + distto:I(log(sl_))),
+					 data = stepID, family= poisson(),
+				map= list(theta = factor(c(NA,1:4))),
+				start = list(theta =c(log(1000), seq(0,0, length.out = 4)))
+					)
+summary(mod.1)
+
+# convergence problem --- why?
+stepID[case_ == T, range(distto)]
+stepID[case_ == F, quantile(distto)]
+
+stepID[case_ == F, quantile(sl_)]
+
+summary(stepID$tod_start_)
+
+# try simplifying -- rm intx sl X distto
+mod.2 <- glmmTMB(case_ ~ I(log(sl_)) + I(log(sl_)):tod_start_ + distto +
+							 	(1|indiv_step_id) +
+							 	(0+ I(log(sl_))|id) +
+							 	(0+ I(log(sl_)):tod_start_|id) +
+							 	(0+ distto) ,
+							 data = stepID, family= poisson(),
+							 map= list(theta = factor(c(NA,1:4))),
+							 start = list(theta =c(log(1000), seq(0,0, length.out = 4)))
+)
+summary(mod.2)
+
