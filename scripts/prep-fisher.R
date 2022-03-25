c11cd147f98a57c6ed4e90233766115663f15f8d
Julie Turner <julwturner@gmail.com>
2022-03-25 10:58:47 -0230

    data with more fishers

--- scripts/prep-fisher.R
+++ scripts/prep-fisher.R
@@ -2,21 +2,25 @@ library(sf)
 library(data.table)
 library(terra)
 
-data('amt_fisher', package = 'amt')
+fishers <- fread(file.path('input', 'fisher_data.csv'))
+summary(as.factor(fishers$id))
+# only 4 individuals in amt data, need more for glmm
+# data('amt_fisher', package = 'amt')
+amt_fisher <- fishers
 
-
-crs <- attr(amt_fisher, 'crs_')
+#crs <- attr(amt_fisher, 'crs_')
+crs <- st_crs(4326)
 outcrs <- st_crs(32618)
 
-sffish <- st_as_sf(amt_fisher, coords = c('x_', 'y_'),
+sffish <- st_as_sf(amt_fisher, coords = c('x', 'y'),
 									 crs = crs)
 
 outfish <- st_transform(sffish, outcrs)
 st_write(outfish, 'input/fisher.csv',
-				 layer_options = "GEOMETRY=AS_XY")
+				 layer_options = "GEOMETRY=AS_XY", append = F)
 
 DT <- fread('input/fisher.csv')
-setnames(DT, c('X', 'Y'), c('x_', 'y_'))
+setnames(DT, c('x', 'y', 't'), c('x_', 'y_','t_'))
 fwrite(DT, 'input/fisher.csv')
 
 data("amt_fisher_covar", package = 'amt')
