45fe78cfd84156960feb7d1740e373d71303e0e7
Julie Turner <julwturner@gmail.com>
2022-03-30 13:20:56 -0230

    clean up toy models

--- scripts/explore-results.R
+++ scripts/explore-results.R
@@ -28,9 +28,6 @@ stepID[description %in% c('cultivated', 'developed'), lc_adj := 'disturbed']
 stepID[description %in% c('barren', 'herbaceous', 'shrubland', 'water'), lc_adj := 'open']
 stepID[description == 'wetlands', lc_adj := 'wetlands']
 
-stepID[,forest := ifelse(description=='forest',1,0)]
-stepID[,wetlands := ifelse(description=='wetlands',1,0)]
-stepID[,disturbed := ifelse(lc_adj=='disturbed',1,0)]
 
 summary(as.factor(stepID$lc_adj))
 
@@ -42,45 +39,38 @@ stepID[, distto_water := units::drop_units(distto_water)]
 summary(stepID$id)
 
 
-mod.1 <- glmmTMB(case_ ~ -1 + I(log(sl_)) +
-								 	I(log(sl_)):tod_start_ +
+### landcover model
+mod.a <- glmmTMB(case_ ~ -1 + I(log(sl_)) +
+								 	I(log(sl_)):lc_adj +
+								 	lc_adj +
 								 	I(log(distto_water+1)) +
 								 	I(log(distto_water+1)):I(log(sl_)) +
-					(1|indiv_step_id) +
-					(0+ I(log(sl_))|id) +
-					(0+ I(log(sl_)):tod_start_|id) +
-					(0+ I(log(distto_water+1))|id) +
-					(0+ I(log(distto_water+1)):I(log(sl_))|id),
-					 data = stepID, family= poisson(),
-				map= list(theta = factor(c(NA,1:6))),
-				start = list(theta =c(log(1000), seq(0,0, length.out = 6)))
-					)
-summary(mod.1)
-
-# can't estimate error --- why?
-stepID[case_ == T, range(distto_water)]
-stepID[case_ == F, quantile(distto_water)]
-
-stepID[case_ == F, quantile(sl_)]
-
-summary(stepID$tod_start_)
-
-# try simplifying -- rm intx sl X distto
-mod.2 <- glmmTMB(case_ ~ -1 + I(log(sl_)) +
-								 	I(log(sl_)):tod_start_ +
-								 	I(log(distto_water+1)) +
 								 	(1|indiv_step_id) +
 								 	(0+ I(log(sl_))|id) +
-								 	(0+ I(log(sl_)):tod_start_|id) +
-								 	(0+ I(log(distto_water+1))|id),
+								 	(0+ I(log(sl_)):lc_adj|id) +
+								 	(0+ lc_adj|id) +
+								 	(0+ I(log(distto_water+1))|id) +
+								 	(0+ I(log(distto_water+1)):I(log(sl_))|id),
 								 data = stepID, family= poisson(),
-								 map= list(theta = factor(c(NA,1:5))),
-								 start = list(theta =c(log(1000), seq(0,0, length.out = 5)))
+								 map= list(theta = factor(c(NA,1:23))),
+								 start = list(theta =c(log(1000), seq(0,0, length.out = 23)))
 )
-summary(mod.2)
+summary(mod.a)
+
+## Doesn't converge, let's check out
+# DT <- stepID[case_=='FALSE', n_by_case  := .N, by = .(lc_adj)]
+# unique(DT[, n_by_case / .N, by = lc_adj])
+unique(stepID[case_=='FALSE', n_by_case  :=  .N, by = .(lc_adj)][,  n_by_case / .N, by = .(lc_adj)]
+)
+stepID[case_=='FALSE',quantile(distto_water)]
+
+# issue could be too many levels for amount of data
+# so lets simplify to just the lc varibales that I decided were interesting to our pretend question
+stepID[,forest := ifelse(description=='forest',1,0)]
+stepID[,disturbed := ifelse(lc_adj=='disturbed',1,0)]
 
 ### forest model
-mod.a <- glmmTMB(case_ ~ -1 + I(log(sl_)) +
+mod.b <- glmmTMB(case_ ~ -1 + I(log(sl_)) +
 								 	I(log(sl_)):forest +
 								 	forest +
 								 	I(log(sl_)):disturbed +
@@ -99,9 +89,6 @@ mod.a <- glmmTMB(case_ ~ -1 + I(log(sl_)) +
 								 map= list(theta = factor(c(NA,1:7))),
 								 start = list(theta =c(log(1000), seq(0,0, length.out = 7)))
 )
-summary(mod.a)
+summary(mod.b)
 
-DT <- stepID[case_=='FALSE', n_by_case  := .N, by = .(lc_adj)]
-unique(DT[, n_by_case / .N, by = lc_adj])
-unique(stepID[case_=='FALSE', n_by_case  :=  .N, by = .(lc_adj)][,  n_by_case / .N, by = .(lc_adj)]
-)
+
