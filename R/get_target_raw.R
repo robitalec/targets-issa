6b1b604b7bc101be799306168b2fcde3ca51dd5a
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 15:09:09 -0700

    fst get_target_raw to simplify

---
+++ R/get_target_raw.R
@@ -0,0 +1,14 @@
+get_target_raw <- function(name, command) {
+	tar_target_raw(
+		name = name,
+		command = command,
+		packages = targets::tar_option_get("packages"),
+		library = targets::tar_option_get("library"),
+		format = "qs",
+		error = targets::tar_option_get("error"),
+		memory = targets::tar_option_get("memory"),
+		garbage_collection = targets::tar_option_get("garbage_collection"),
+		deployment = targets::tar_option_get("deployment"),
+		priority = targets::tar_option_get("priority")
+	)
+}
