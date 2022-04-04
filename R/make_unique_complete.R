9762d7b7ddf15c9c8c4b706b234304f2b7815877
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 13:44:26 -0700

    rm make_unique_complete

--- R/make_unique_complete.R
+++
@@ -1,5 +0,0 @@
-# Make unique and complete ------------------------------------------------
-make_unique_complete <- function(DT, id, datetime, long, lat) {
-	na.omit(unique(DT, by = c(id, datetime)),
-					cols = c(long, lat, datetime))
-}
