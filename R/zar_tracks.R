5640e27c1e960c2679cc4a1f0ca0f40ba19fc6e1
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 14:57:25 -0700

    full deparse substitute version

--- R/zar_tracks.R
+++ R/zar_tracks.R
@@ -1,23 +1,67 @@
-zar_tracks <- function(crs_sp, rate, tolerance) {
-	# sym_file <- as.symbol(name_file)
-	# command_data <- substitute(read_data(file), env = list(file = sym_file))
+#' zar_tracks
+#'
+#' @return
+#' @export
+#'
+#' @examples
+zar_tracks <-
+	function(data,
+					 packages = targets::tar_option_get("packages"),
+					 library = targets::tar_option_get("library"),
+					 format = "qs",
+					 error = targets::tar_option_get("error"),
+					 memory = targets::tar_option_get("memory"),
+					 garbage_collection = targets::tar_option_get("garbage_collection"),
+					 deployment = targets::tar_option_get("deployment"),
+					 priority = targets::tar_option_get("priority"),
+					 cue = targets::tar_option_get("cue")) {
+		name <- 'locs'
 
-	c(
-		tar_target(
-			tracks,
-			make_track(prep_locs, x_, y_, t_, all_cols = TRUE, crs = crs_sp)
-		),
-		# tar_target_raw(
-		# 	'tracks',
-		# 	substitute(
-		# 		make_track(DT_sym, x_, y_, t_, all_cols = TRUE, crs = crs_sp),
-		# 		env = list(DT_sym = deparse(substitute(name)))
-		# 	)
-		# ),
-		tar_target(
-			resamples,
-			resample_tracks(tracks, rate = rate, tolerance = tolerance),
-			pattern = map(tracks)
+		name_tracks_raw <- paste(name, 'tracks', 'raw', sep = '_')
+		name_tracks <- paste(name, 'tracks', sep = '_')
+		name_resample <- paste(name, 'resample', sep = '_')
+		name_random <- paste(name, 'random', sep = '_')
+
+
+		command_tracks_raw <- substitute(data)
+		command_tracks <- substitute(make_track(tracks, x_, y_, t_, all_cols = TRUE, crs = crs_sp),
+																 env = list(tracks = as.symbol(name_tracks_raw)))
+		# command_resample <- substitute(brms::brmsformula(formula))
+		# command_random <- substitute(priors)
+
+
+		target_tracks_raw <- tar_target_raw(
+			name = name_tracks_raw,
+			command = command_tracks_raw,
+			packages = packages,
+			library = library,
+			format = format,
+			error = error,
+			memory = memory,
+			garbage_collection = garbage_collection,
+			deployment = deployment,
+			priority = priority,
+			cue = cue
 		)
-	)
-}
+		target_tracks <- tar_target_raw(
+			name = name_tracks,
+			command = command_tracks,
+			packages = packages,
+			library = library,
+			format = format,
+			error = error,
+			memory = memory,
+			garbage_collection = garbage_collection,
+			deployment = deployment,
+			priority = priority,
+			cue = cue
+		)
+
+		return(list(target_tracks_raw,
+								target_tracks
+								# target_data,
+								# target_sample_tracks,
+								# target_stancode,
+								# target_sample))
+		))
+								}
