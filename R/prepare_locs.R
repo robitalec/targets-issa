bed982c8c7b40a918d267fae916c8fcdeb1fc4b7
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 13:40:27 -0700

    fix xy/long lat, add tar_group

--- R/prepare_locs.R
+++ R/prepare_locs.R
@@ -1,4 +1,4 @@
-prepare_locs <- function(DT, id, datetime, tz, long, lat) {
+prepare_locs <- function(DT, id, datetime, tz, x, y, split_by) {
 	stopifnot(is.character(datetime))
 
 	DT[, (datetime) := parse_date(datetime_char, default_tz = tz),
@@ -6,7 +6,10 @@ prepare_locs <- function(DT, id, datetime, tz, long, lat) {
 
 	# Make unique and complete
 	unique_DT <- unique(DT, by = c(id, datetime))
-	na_omit_DT <- na.omit(unique_DT, cols = c(long, lat, datetime))
+	na_omit_DT <- na.omit(unique_DT, cols = c(x, y, datetime))
+
+	# Make splits
+	DT[, tar_group := .GRP, by = c(split_by)]
 
 	return(na_omit_DT)
 }
