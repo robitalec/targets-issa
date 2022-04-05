32901325720d595bab4a741b0e36539f593c4b0c
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-05 10:32:04 -0700

    drop calc rss from plot

--- R/plot_rss.R
+++ R/plot_rss.R
@@ -1,10 +1,5 @@
-plot_rss <- function(pred_h1, pred_h2, theme) {
-	# TODO: fix this column subsetting
-	log_rss <- merge(pred_h1[, .(id, h1_forest)], pred_h2[, .(id, h2)],
-									 by = 'id', all.x = TRUE)
-	log_rss[, rss := h1_forest - h2]
-
-	ggplot(data = log_rss, aes(x, rss)) +
+plot_rss <- function(rss, theme) {
+	ggplot(data = rss, aes(x, rss)) +
 		geom_line(aes(group = id , alpha = .0001),
 							linetype = 'twodash',
 							show.legend = F) +
@@ -15,8 +10,6 @@ plot_rss <- function(pred_h1, pred_h2, theme) {
 			lty = 2,
 			size = .7
 		) +
-		ylab("logRSS") + xlab("Forest") +
-		ggtitle("RSS compared to 0 forest") + theme(plot.title = element_text(hjust = 0.5)) +
 		scale_color_colorblind()  +
 		scale_fill_colorblind() +
 		plot_theme()
