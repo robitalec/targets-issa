1d25bdffa35d0de343151d31b50298d80f48c377
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:32:20 -0700

    fst model_lc

---
+++ R/model_lc.R
@@ -0,0 +1,19 @@
+model_lc <- function(DT) {
+	glmmTMB(
+		case_ ~ -1 + I(log(sl_)) +
+			I(log(sl_)):lc_adj +
+			lc_adj +
+			I(log(distto_water + 1)) +
+			I(log(distto_water + 1)):I(log(sl_)) +
+			(1 | indiv_step_id) +
+			(0 + I(log(sl_)) | id) +
+			(0 + I(log(sl_)):lc_adj | id) +
+			(0 + lc_adj | id) +
+			(0 + I(log(distto_water + 1)) | id) +
+			(0 + I(log(distto_water + 1)):I(log(sl_)) | id),
+		data = DT,
+		family = poisson(),
+		map = list(theta = factor(c(NA, 1:23))),
+		start = list(theta = c(log(1000), seq(0, 0, length.out = 23)))
+	)
+}
