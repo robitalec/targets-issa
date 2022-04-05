0314ccc88de6454d84b62e64d554dbc42308c0a0
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:00:33 -0700

    wrapper extract_layers

---
+++ R/extract_layers.R
@@ -0,0 +1,12 @@
+extract_layers <- function(DT, crs, lc, elev, popdens, water) {
+	setDT(DT)
+
+	start <- c('x1_', 'y1_')
+	end <- c('x2_', 'y2_')
+
+	extract_pt(DT, lc, end)
+	extract_pt(DT, elev, end)
+	extract_pt(DT, popdens, end)
+
+	extract_distance_to(DT, water, end, crs)
+}
