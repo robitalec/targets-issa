ec24ffcd965e008ac8b958aae72431cbcf64d0d9
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:13:25 -0700

    return df

--- R/calc_distribution_parameters.R
+++ R/calc_distribution_parameters.R
@@ -1,4 +1,4 @@
 calc_distribution_parameters <- function(steps) {
 	# if (is.null(steps)) return()
-	c(ta_distr_params(steps), sl_distr_params(steps))
+	data.frame(ta_distr_params(steps), sl_distr_params(steps))
 }
