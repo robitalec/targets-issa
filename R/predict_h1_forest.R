b9bdb8c844866d1b57e562c7cbb32492c2c69deb
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-05 11:07:55 -0700

    use consistent N

--- R/predict_h1_forest.R
+++ R/predict_h1_forest.R
@@ -2,9 +2,11 @@
 #' @export
 #' @author Julie W. Turner, Alec L. Robitaille
 predict_h1_forest <- function(DT, model) {
+	N <- 100L
+
 	new_data <- DT[, .(
 		sl_ = mean(sl_),
-		forest = seq(from = 0, to = 1, length.out = 100),
+		forest = seq(from = 0, to = 1, length.out = N),
 		disturbed = 0,
 		dist_to_water= median(dist_to_water, na.rm = T),
 		indiv_step_id = NA
@@ -12,6 +14,5 @@ predict_h1_forest <- function(DT, model) {
 
 	new_data[, h1_forest := predict(model, .SD, type = 'link', re.form = NULL)]
 
-	# TODO: check lengths
-	# new_data[, x := seq(from = 0, to = 1, length.out = 100)]
+	new_data[, x := seq(from = 0, to = 1, length.out = N), by = id]
 }
--- R/predict_h1_water.R
+++ R/predict_h1_water.R
@@ -2,16 +2,18 @@
 #' @export
 #' @author Julie W. Turner, Alec L. Robitaille
 predict_h1_water <- function(DT, model) {
+	N <- 100L
+
 	new_data <- DT[, .(
 		sl_ = mean(sl_),
 		forest = 0,
 		disturbed = 0,
-		dist_to_water = seq(from = 0, to = 1500, length.out = 100),
+		dist_to_water = seq(from = 0, to = 1500, length.out = N),
 		indiv_step_id = NA
 	), by = id]
 
 	new_data[, h1_water := predict(model, .SD, type = 'link', re.form = NULL)]
 
 	# TODO: check lengths
-	# new_data[, x :=  seq(from = 0, to = 1500, length.out = 100)]
+	new_data[, x :=  seq(from = 0, to = 1500, length.out = N), by = id]
 }
