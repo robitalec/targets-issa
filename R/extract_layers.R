654fe6a65291be8b47e1352981309a9281878a05
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:04:55 -0700

    merge legend

--- R/extract_layers.R
+++ R/extract_layers.R
@@ -1,10 +1,12 @@
-extract_layers <- function(DT, crs, lc, elev, popdens, water) {
+extract_layers <- function(DT, crs, lc, legend, elev, popdens, water) {
 	setDT(DT)
 
 	start <- c('x1_', 'y1_')
 	end <- c('x2_', 'y2_')
 
 	extract_pt(DT, lc, end)
+	DT[legend, lc_description := description, on = .(lc = Value)]
+
 	extract_pt(DT, elev, end)
 	extract_pt(DT, popdens, end)
 
