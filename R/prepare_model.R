1bc6dcb0b673143105a6e5533345bc5a3c033836
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:30:24 -0700

    fix names

--- R/prepare_model.R
+++ R/prepare_model.R
@@ -1,15 +1,15 @@
 prepare_model <- function(DT) {
 	DT[, indiv_step_id := paste(id, step_id_, sep = '_')]
 
-	DT[description == 'forest', lc_adj := 'forest']
-	DT[description %in% c('cultivated', 'developed'), lc_adj := 'disturbed']
-	DT[description %in% c('barren', 'herbaceous', 'shrubland', 'water'), lc_adj := 'open']
-	DT[description == 'wetlands', lc_adj := 'wetlands']
+	DT[lc_description == 'forest', lc_adj := 'forest']
+	DT[lc_description %in% c('cultivated', 'developed'), lc_adj := 'disturbed']
+	DT[lc_description %in% c('barren', 'herbaceous', 'shrubland', 'water'), lc_adj := 'open']
+	DT[lc_description == 'wetlands', lc_adj := 'wetlands']
 
-
-	DT[, lc := as.factor(lc)]
+	DT[, pt_lc := as.factor(pt_lc)]
 	DT[, lc_adj := as.factor(lc_adj)]
 	DT[, indiv_step_id := as.factor(indiv_step_id)]
 	DT[, id := as.factor(id)]
-	DT[, distto_water := units::drop_units(distto_water)]
+
+	DT[, dist_to_water := units::drop_units(dist_to_water)]
 }
