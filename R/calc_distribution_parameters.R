9c1fdee7485b188c1f46d4bfb82cbb80abc41198
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:22:03 -0700

    also id

--- R/calc_distribution_parameters.R
+++ R/calc_distribution_parameters.R
@@ -1,6 +1,7 @@
 calc_distribution_parameters <- function(steps) {
 	# if (is.null(steps)) return()
 	data.frame(
+		id = steps$id[[1]],
 		tar_group = steps$tar_group[[1]],
 		ta_distr_params(steps),
 		sl_distr_params(steps)
