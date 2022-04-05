870e2ff100bb4ec9d313a29714a1987822f71c5d
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-05 10:32:53 -0700

    rm redundant

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -5,11 +5,9 @@ plot_theme <- function() {
 		panel.grid.minor = element_blank(),
 		axis.line = element_line(colour = "black", size = .7),
 		plot.title = element_text(size = 12, hjust = 0.05),
-		axis.text.x = element_text(size = 12),
 		axis.title = element_text(size = 15),
-		axis.text.y = element_text(size = 12),
-		axis.text.x = element_text(margin = margin(10, 10, 10, 10, "pt")),
-		axis.text.y = element_text(margin = margin(10, 10, 10, 10, "pt")),
+		axis.text.x = element_text(size = 12, margin = margin(10, 10, 10, 10, "pt")),
+		axis.text.y = element_text(size = 12, margin = margin(10, 10, 10, 10, "pt")),
 		axis.ticks.length = unit(-0.25, "cm")
 	)
 }
