f309b09c45d7ecae2ac6d9f078d32033daec1dc9
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-05 09:56:29 -0700

    fst predict_h1_water

---
+++ R/predict_h1_water.R
@@ -0,0 +1,15 @@
+#' @title Predict H1 distance to water
+#' @export
+#' @author Julie W. Turner, Alec L. Robitaille
+predict_h1_water <- function(DT, model) {
+	new_data <- DT[, .(
+		sl_ = mean(sl_),
+		forest = 0,
+		disturbed = 0,
+		dist_to_water = seq(from = 0, to = 1500, length.out = 100),
+		indiv_step_id = NA,
+		id = .BY[[1]]
+	), by = id]
+
+	new_data[, h1_water := predict(model, .SD, type = 'link', re.form = NULL)]
+}
