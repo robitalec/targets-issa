6a318786fd563846711de73293a8f3a64df1e929
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:27:31 -0700

    fill prepare_model

--- R/prepare_model.R
+++ R/prepare_model.R
@@ -1,4 +1,15 @@
-function() {
-	# prep_model
-	# [,indiv_step_id := paste(id, step_id_, sep = '_')]
+prepare_model <- function(DT) {
+	DT[, indiv_step_id := paste(id, step_id_, sep = '_')]
+
+	DT[description == 'forest', lc_adj := 'forest']
+	DT[description %in% c('cultivated', 'developed'), lc_adj := 'disturbed']
+	DT[description %in% c('barren', 'herbaceous', 'shrubland', 'water'), lc_adj := 'open']
+	DT[description == 'wetlands', lc_adj := 'wetlands']
+
+
+	DT[, lc := as.factor(lc)]
+	DT[, lc_adj := as.factor(lc_adj)]
+	DT[, indiv_step_id := as.factor(indiv_step_id)]
+	DT[, id := as.factor(id)]
+	DT[, distto_water := units::drop_units(distto_water)]
 }
