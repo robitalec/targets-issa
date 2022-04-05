2869e807a048efc8b382d401b92c21cea174c992
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-04 17:36:24 -0700

    bin land cover forest/not

--- R/prepare_model.R
+++ R/prepare_model.R
@@ -6,6 +6,9 @@ prepare_model <- function(DT) {
 	DT[lc_description %in% c('barren', 'herbaceous', 'shrubland', 'water'), lc_adj := 'open']
 	DT[lc_description == 'wetlands', lc_adj := 'wetlands']
 
+	DT[, forest := ifelse(description == 'forest', 1, 0)]
+	DT[, disturbed := ifelse(lc_adj == 'disturbed', 1, 0)]
+
 	DT[, pt_lc := as.factor(pt_lc)]
 	DT[, lc_adj := as.factor(lc_adj)]
 	DT[, indiv_step_id := as.factor(indiv_step_id)]
