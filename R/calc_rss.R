c59fbe24a8a88d87c1d336357db672a7371606b8
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-07 10:17:55 -0700

    fin todo

--- R/calc_rss.R
+++ R/calc_rss.R
@@ -2,7 +2,6 @@
 #' @export
 #' @author Julie W. Turner, Alec L. Robitaille
 calc_rss <- function(pred_h1, h1_col, pred_h2, h2_col) {
-	# TODO: fix this column subsetting
 	log_rss <- merge(pred_h1[, .SD, .SDcols = c('id', 'x', h1_col)],
 									 pred_h2[, .SD, .SDcols = c('id', h2_col)],
 									 by = 'id', all.x = TRUE)
