c09dc34f88a6927efbf848f128aa83b2aec9979f
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-07 13:51:03 -0700

    add calc availability with flex on type

--- R/archive/calc_availability.R
+++ R/archive/calc_availability.R
@@ -1,5 +1,22 @@
-# Calculate availability ---------------------------------------
-#TODO ***
+calc_availability <- function(DT, col, type, split_by) {
+	DT_avail <- DT[!(case_)]
+
+	if (type == 'proportion') {
+		DT[, n_split_by := .N, c(split_by)]
+		DT[, (paste0('prop_', col)) := .N / n_split_by, c(col, split_by)]
+	} else if (type == 'mean') {
+		DT[, (paste0('mean_', col)) := mean(x), c(split_by), env = list(x = col)]
+	} else if (type == 'median') {
+		DT[, (paste0('median_', col)) := median(x), c(split_by), env = list(x = col)]
+	}
+
+}
+
+
+
+
+
+
 calc_availability <- function(DT, params) {
 	if (is.null(DT)) return()
 	sum.DT <- data.table()
@@ -9,3 +26,8 @@ calc_availability <- function(DT, params) {
 	})
 
 }
+
+
+
+# habitat avail to individual
+# s
