7ea89c720ab5099370913be3f530d8441c6d8c46
Alec L. Robitaille <robit.alec@gmail.com>
2022-04-07 14:46:45 -0700

    tidy

--- R/calc_availability.R
+++ R/calc_availability.R
@@ -11,23 +11,3 @@ calc_availability <- function(DT, col, type, split_by) {
 	}
 
 }
-
-
-
-
-
-
-calc_availability <- function(DT, params) {
-	if (is.null(DT)) return()
-	sum.DT <- data.table()
-	lapply(params,function(){
-		if(is.factor(DT[,.(params)]))
-			sum.DT[,paste('availavility', params, sep = '_') := DT[,sum(.SD[,.N])]]
-	})
-
-}
-
-
-
-# habitat avail to individual
-# s
