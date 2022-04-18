19f46dd63d8fa2400105004325404e4a289cd330
Julie Turner <julwturner@gmail.com>
2022-04-18 13:31:31 -0230

    edit prepare_speed

--- R/prepare_speed.R
+++ R/prepare_speed.R
@@ -7,7 +7,7 @@ prep_speed <- function(DT, summary, params) {
 																 max.water = max(dist_to_water, na.rm = T)),
 															by = .(id)]
 
-	dat.wide <- dcast(summary =indiv_summary[term %like% 'sl'], id~ term, value.var = 'estimate')
+	dat.wide <- dcast(summary[term %like% 'sl'], id~ term, value.var = 'estimate')
 
 	dat.wide <- setDT(merge(dat.wide, setDT(params)[,.(id = as.character(id),shape, scale, kappa)], by = 'id', all.x = T))
 	dat.wide <- setDT(merge(dat.wide, sum.distwater, by = 'id', all.x = T))
