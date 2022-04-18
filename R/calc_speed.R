769384a841e2c31381b9608e79b0226f56a78b10
Julie Turner <julwturner@gmail.com>
2022-04-18 13:31:51 -0230

    edit calc_speed

--- R/calc_speed.R
+++ R/calc_speed.R
@@ -2,14 +2,31 @@
 #' @export
 #' @author Julie W. Turner
 calc_speed <- function(DT, covariate, seq) {
+	if(covariate == "forest")
 	DT[, `:=` (spd = list(list((shape +`I(log(sl_))` +
-																			ifelse(covariate == 'forest', `I(log(sl_)):forest`*seq, `I(log(sl_)):forest`) +
-																			ifelse(covariate == 'disturbed',`I(log(sl_)):disturbed`*seq, `I(log(sl_)):disturbed`) +
-																			ifelse(covariate == 'dist_to_water', `I(log(dist_to_water + 1)):I(log(sl_))`*seq,
-																						 `I(log(dist_to_water + 1)):I(log(sl_))`*median.water)
+																			`I(log(sl_)):forest`*seq +
+																			`I(log(sl_)):disturbed` +
+																			`I(log(dist_to_water + 1)):I(log(sl_))`*median.water
 															)*(scale))),
 									 x = list(list(seq))),
 					 by=.(id)]
-	move <- DT[, .(spd = unlist(spd), x = unlist(x)), by=.(id)]
+	if(covariate == "disturbed")
+		DT[, `:=` (spd = list(list((shape +`I(log(sl_))` +
+																	`I(log(sl_)):forest` +
+																	`I(log(sl_)):disturbed`*seq +
+																	`I(log(dist_to_water + 1)):I(log(sl_))`*median.water
+		)*(scale))),
+		x = list(list(seq))),
+		by=.(id)]
+	if(covariate == "dist_to_water")
+		DT[, `:=` (spd = list(list((shape +`I(log(sl_))` +
+																	`I(log(sl_)):forest` +
+																	`I(log(sl_)):disturbed`+
+																	`I(log(dist_to_water + 1)):I(log(sl_))`*seq
+		)*(scale))),
+		x = list(list(seq))),
+		by=.(id)]
 
+	move <- DT[, .(spd = unlist(spd), x = unlist(x)), by=.(id)]
+	move
 }
