13c18da78f8d0f98208ec0d2199b34e8b2982894
Julie Turner <julwturner@gmail.com>
2022-04-18 13:18:41 -0230

    edit calc_speed

--- R/calc_speed.R
+++ R/calc_speed.R
@@ -4,7 +4,7 @@
 calc_speed <- function(DT, covariate, seq) {
 	DT[, `:=` (spd = list(list((shape +`I(log(sl_))` +
 																			ifelse(covariate == 'forest', `I(log(sl_)):forest`*seq, `I(log(sl_)):forest`) +
-																			ifelse(covariate == 'disturbance',`I(log(sl_)):disturbed`*seq, `I(log(sl_)):disturbed`) +
+																			ifelse(covariate == 'disturbed',`I(log(sl_)):disturbed`*seq, `I(log(sl_)):disturbed`) +
 																			ifelse(covariate == 'dist_to_water', `I(log(dist_to_water + 1)):I(log(sl_))`*seq,
 																						 `I(log(dist_to_water + 1)):I(log(sl_))`*median.water)
 															)*(scale))),
