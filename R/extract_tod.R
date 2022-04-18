60e599f6d75ae7cfa9884b3bae87bdb5911d36d9
Julie Turner <julwturner@gmail.com>
2022-04-18 16:45:01 -0230

    edit extract tod for start

--- R/extract_tod.R
+++ R/extract_tod.R
@@ -1,5 +1,5 @@
 extract_tod <- function(DT, solar_dep = 6, include_crepuscule = TRUE, coords) {
 	# needs st_transform 4326
-	pts <- DT[, sunriset(as.matrix(cbind(.SD)), t2_), .SDcols = coords]
+	pts <- DT[, sunriset(as.matrix(cbind(.SD)), t1_), .SDcols = coords]
 }
 
