ad055eaef2bf0b694b8b0ae34a23c85cf6356d28
Julie Turner <julwturner@gmail.com>
2022-04-18 13:18:23 -0230

    update boxplots speed vs covs

--- R/plot_boxplot.R
+++ R/plot_boxplot.R
@@ -1,13 +1,13 @@
 #' @title Plot boxplot
 #' @export
 #' @author Julie W. Turner
-plot_box <- function(model, theme) {
+plot_box<- function(DT, theme) {
 
-	ggplot(data = model[term !='(Intercept)'],
-				 aes(term, estimate)) +
-		geom_boxplot() +
-		geom_jitter() +
-		geom_hline(yintercept = 0, lty = 'dashed') +
-		coord_flip() +
+	ggplot(data = DT,
+				 aes(as.factor(x), spd)) +
+		geom_boxplot(aes(color = as.factor(x))) +
+		geom_jitter(aes(color = as.factor(x))) +
+		scale_color_colorblind()  +
+		scale_fill_colorblind() +
 		plot_theme()
 }
