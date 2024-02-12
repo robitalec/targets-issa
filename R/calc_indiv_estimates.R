bf44147f19543cc1bb945a4801e2a7521239fc5f
Alec L. Robitaille <robit.alec@gmail.com>
2024-02-12 11:08:28 -0400

    fix cast to data.table

--- R/calc_indiv_estimates.R
+++ R/calc_indiv_estimates.R
@@ -2,7 +2,9 @@
 #' @export
 #' @author Julie W. Turner
 indiv_estimates <- function(model) {
-	setDT(coef(model)$cond$id %>% rownames_to_column("id") %>%
-									pivot_longer(-id, names_to = "term", values_to = "estimate") %>%
-									mutate(method = "ME"))
+	as.data.table(
+		coef(model)$cond$id %>% rownames_to_column("id") %>%
+			pivot_longer(-id, names_to = "term", values_to = "estimate") %>%
+			mutate(method = "ME")
+	)
 }
